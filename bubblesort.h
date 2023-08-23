2a15a35bee8510df3cc12462fc7be0a05d7947c8
HamzLDN <75676416+HamzLDN@users.noreply.github.com>
2023-08-23 18:10:10 +0100

    Update bubblesort.h

--- bubblesort.h
+++ bubblesort.h
@@ -4,7 +4,7 @@ void bubbleSort(int arr[], int size) {
         // Used to iterate over the needed values using size - i - 1 which helps with optimization
         for (int j=0;j<size-i-1;j++) {
             // Check if the current value is greater than the next value (n+1)
-            if(arr[j]<arr[j+1]) {
+            if(arr[j]>arr[j+1]) {
                 // Creates a temporary variable and swaps the values
                 int t=arr[j+1];
                 arr[j+1]=arr[j];
